a1a8454747d8b37bf7857de84412ac9c78ec9bb0
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-10-19 13:00:58 +0530

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',
+{ useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb');
+  }
+
+  console.log('connected to mongodb!!');
+  const db = client.db('TodoApp');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({ _id: new ObjectID('5bc985e4b534dd22ce37fe64') })
+  .then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').deleteMany({ name: 'Avadh' });
+
+  //client.close();
+});
