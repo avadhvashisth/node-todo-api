cadc593cff5ce879d03eba4d565b352ecdcf0916
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-10-24 19:59:50 +0530

    setting up mlab and localhost url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
+var url = 'mongodb://localhost:27017/TodoApp';
+if (process.env.PORT) {
+  url = 'mongodb://Avadh vashisth:avadh123@ds239873.mlab.com:39873/node-todo-api';
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Avadh vashisth:avadh123@ds239873.mlab.com:39873/node-todo-api', { useNewUrlParser: true });
+mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = {
   mongoose,
