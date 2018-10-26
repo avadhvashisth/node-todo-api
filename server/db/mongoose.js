8d3dc4e00235ceaa291b67df9b03a14f4d62736d
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-10-26 13:08:33 +0530

    setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,7 @@
 var mongoose = require('mongoose');
 
-var url = 'mongodb://localhost:27017/TodoApp';
-if (process.env.PORT) {
-  url = 'mongodb://Avadh vashisth:avadh123@ds239873.mlab.com:39873/node-todo-api';
-}
-
 mongoose.Promise = global.Promise;
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = {
   mongoose,
