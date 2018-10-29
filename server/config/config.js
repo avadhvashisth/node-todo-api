704678225bac091cd06d879f50e1c28d61851557
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-10-29 18:53:26 +0530

    Add POST /users route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -12,5 +12,6 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://Avadh vashisth:avadh123@ds239873.mlab.com:39873/node-todo-api';
+  process.env.MONGODB_URI = 'mongodb://Avadh vashisth:avadh123@ds239873.mlab.com' +
+  ':39873/node-todo-api';
 }
--- server/server.js
+++ server/server.js
@@ -98,6 +98,19 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => user.generateAuthToken())
+  .then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    console.log(e);
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on PORT: ${port}`);
 });
