c61092a4d38bddbf97fecbd17a3fa2ad59127ca1
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-11-09 23:29:15 +0530

    setting env

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,11 @@ if (process.env.NODE_ENV != 'test') {
 
 console.log('env *****', env);
 
-var config = require('./config.json');
-var envConfig = config[env];
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
 
-Object.keys(envConfig).forEach((key) => {
-  process.env[key] = envConfig[key];
-});
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
