142b2aebd437edcf80efc577c3313eaa5581f3d9
Avadh Vashisth <avadh.vashisth@gmail.com>
2018-11-09 23:07:25 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -5,13 +5,9 @@ if (process.env.NODE_ENV != 'test') {
 
 console.log('env *****', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-} else if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://Avadh vashisth:avadh123@ds239873.mlab.com' +
-  ':39873/node-todo-api';
-}
+var config = require('./config.json');
+var envConfig = config[env];
+
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
